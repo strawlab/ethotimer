03aa140ad14d6b92baf28fa48b0107cc8298971c
Andrew Straw <strawman@astraw.com>
2021-12-14 23:11:35 +0100

    windows script stops on error

--- build.bat
+++ build.bat
@@ -1,9 +1,15 @@
 REM Install wasm-pack from here https://rustwasm.github.io/wasm-pack/installer/
-wasm-pack build --target web
+wasm-pack build --target web || goto :error
 
 mkdir deploy
-copy pkg\* deploy
-copy static\* deploy
+copy pkg\* deploy || goto :error
+copy static\* deploy || goto :error
 
 REM Build OK. Now run with:
 REM     microserver --port 8000 --no-spa deploy
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
